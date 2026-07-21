40ae765cd58f95b8b565d86cd7fc4c4d58c7eb08
harlemchu <king.harlemchu@gmail.com>
2026-07-21 12:56:54 +0800

    fix: profiles table migration file changed

--- database/migrations/2026_07_21_043649_create_profiles_table.php
+++ database/migrations/2026_07_21_043649_create_profiles_table.php
@@ -1,25 +1,26 @@
 <?php
+// database/migrations/xxxx_xx_xx_create_profiles_table.php
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->integer('graduation_year');
+            $table->text('bio')->nullable();
+            $table->string('avatar')->nullable();
+            $table->string('class_name')->nullable();
+            $table->string('quote')->nullable();
+            $table->json('interests')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('profiles');
